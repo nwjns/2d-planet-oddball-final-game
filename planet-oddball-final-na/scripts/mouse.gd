ecabcb66d57fca49b84d99a389023d910c1c6722
Keith D. <keithlol420@gmail.com>
2025-06-23 22:16:18 +0800

    added enemies on level 2

--- planet-oddball-final-na/scripts/mouse.gd
+++ planet-oddball-final-na/scripts/mouse.gd
@@ -3,17 +3,15 @@ extends Node2D
 const speed = 90
 
 var direction = 1
-@onready var ray_cast_right = $RayCastRight
-@onready var ray_cast_left = $RayCastLeft
+@onready var ray_cast_down = $RayCastDown
+@onready var ray_cast_up = $RayCastUp
 @onready var animated_sprite = $AnimatedSprite2D
 
 func _process(delta):
-	if ray_cast_right.is_colliding():
+	if ray_cast_down.is_colliding():
 		direction = -1
-		animated_sprite.flip_h = false
 		
-	if ray_cast_left.is_colliding():
+	if ray_cast_up.is_colliding():
 		direction = 1
-		animated_sprite.flip_h = true
 		
-	position.x += direction * speed * delta
+	position.y += direction * speed * delta
