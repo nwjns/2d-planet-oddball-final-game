7d08005886c6d69050ebfc47bc2b9a4667999bc1
Keith D. <keithlol420@gmail.com>
2025-06-23 19:57:23 +0800

    added level 2

--- planet-oddball-final-na/scripts/stardust_counter.gd
+++ planet-oddball-final-na/scripts/stardust_counter.gd
@@ -7,10 +7,6 @@ func _ready() -> void:
 	$stardustCount.text = "Stardusts: " + str(coins_collected)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 
 func _on_stardust_1_body_entered(body: Node2D) -> void:
 	coins_collected = coins_collected + 1
