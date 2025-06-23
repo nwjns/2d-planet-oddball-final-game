b773771d9fc36d84f272a7365d5aede64c3c932d
Keith D. <keithlol420@gmail.com>
2025-06-23 22:02:29 +0800

    added tilemaps, stardust and designs on level 2

--- planet-oddball-final-na/scripts/player.gd
+++ planet-oddball-final-na/scripts/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 const SPEED = 180.0
-const JUMP_VELOCITY = -500.0
+const JUMP_VELOCITY = -550.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
